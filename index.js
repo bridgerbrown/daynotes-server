4a2064334b25dda816a6b9f6ad045369b6e725fd
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-07-19 11:41:49 -0700

    removed function scope for ws, express public

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { MongoClient } = require("mongodb");
 const { Server } = require("socket.io");
 
 const app = express();
-app.use(express.static("public"));
+app.use(express());
 
 app.use(
   cors({
@@ -22,6 +22,7 @@ const wss =
     ? new WebSocket.Server({ server })
     : new WebSocket.Server({ port: 5001 });
 const io = new Server(server);
+console.log("io: " + io);
 
 server.listen(serverPort);
 console.log(`Server started on port ${serverPort} in stage ${process.env.NODE_ENV}`);
@@ -50,90 +51,81 @@ const keepServerAlive = () => {
   }, 50000);
 };
 
-async function startServer() {
-  try {
-    await connectToDatabase();
-    wss.on("connection", (ws, req) => {
-      console.log("Connection Opened");
-      console.log("Client size: ", wss.clients.size);
+wss.on("connection", (ws, req) => {
+  console.log("Connection Opened");
+  console.log("Client size: ", wss.clients.size);
 
-      if (wss.clients.size === 1) {
-        console.log("first connection. starting keepalive");
-        keepServerAlive();
-      }
+  if (wss.clients.size === 1) {
+    console.log("first connection. starting keepalive");
+    keepServerAlive();
+  }
 
-      ws.on("message", (data) => {
-        let stringifiedData = data.toString();
-        if (stringifiedData === "pong") {
-          console.log("keepAlive");
-          return;
-        }
-        broadcast(ws, stringifiedData, false);
-      });
-
-      ws.on("close", (data) => {
-        console.log("closing connection");
-
-        if (wss.clients.size === 0) {
-          console.log("last client disconnected, stopping keepAlive interval");
-          clearInterval(keepAliveId);
-        }
-      });
-    });
-
-    function broadcast(ws, message, includeSelf) {
-      if (includeSelf) {
-        wss.clients.forEach((client) => {
-          if (client.readyState === WebSocket.OPEN) {
-            client.send(message);
-          }
-        });
-      } else {
-        wss.clients.forEach((client) => {
-          if (client !== ws && client.readyState === WebSocket.OPEN) {
-            client.send(message);
-          }
-        });
-      }
+  ws.on("message", (data) => {
+    let stringifiedData = data.toString();
+    if (stringifiedData === "pong") {
+      console.log("keepAlive");
+      return;
     }
+    broadcast(ws, stringifiedData, false);
+  });
 
-    io.on("connect", (socket) => {
-      socket.on("get-document", async (userId, date) => {
-        const documentId = `${userId}-${date}`;
-        const document = await findOrCreateDocument(documentId, userId, date);
-        console.log("Socket event received:", documentId);
-        socket.join(documentId);
-        socket.emit("load-document", document.data);
+  ws.on("close", (data) => {
+    console.log("closing connection");
 
-        socket.on("send-changes", (delta) => {
-          socket.broadcast.to(documentId).emit("receive-changes", delta);
-        });
+    if (wss.clients.size === 0) {
+      console.log("last client disconnected, stopping keepAlive interval");
+      clearInterval(keepAliveId);
+    }
+  });
+});
 
-        socket.on("save-document", async (data) => {
-          await saveDocument(documentId, data);
-        });
-
-        socket.on("delete-note", async () => {
-          await deleteDocument(documentId);
-        });
-
-        socket.on("disconnect", async () => {
-          const note = await findDocument(documentId);
-          if (note && note.data.ops) {
-            const noteData = note.data.ops[0].insert;
-            if (noteData.length === 1 || isOnlyWhiteSpace(noteData)) {
-              await deleteDocument(documentId);
-            }
-          }
-        });
-      });
+function broadcast(ws, message, includeSelf) {
+  if (includeSelf) {
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
     });
-
-  } catch (error) {
-    console.error("Error starting the server:", error);
-    client.close();
+  } else {
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
   }
 }
+
+io.on("connect", (socket) => {
+  socket.on("get-document", async (userId, date) => {
+    const documentId = `${userId}-${date}`;
+    const document = await findOrCreateDocument(documentId, userId, date);
+    console.log("Socket event received:", documentId);
+    socket.join(documentId);
+    socket.emit("load-document", document.data);
+
+      socket.on("send-changes", (delta) => {
+        socket.broadcast.to(documentId).emit("receive-changes", delta);
+      });
+
+      socket.on("save-document", async (data) => {
+        await saveDocument(documentId, data);
+      });
+
+      socket.on("delete-note", async () => {
+        await deleteDocument(documentId);
+      });
+
+      socket.on("disconnect", async () => {
+        const note = await findDocument(documentId);
+        if (note && note.data.ops) {
+          const noteData = note.data.ops[0].insert;
+          if (noteData.length === 1 || isOnlyWhiteSpace(noteData)) {
+            await deleteDocument(documentId);
+          }
+        }
+      });
+    });
+  });
 
 async function findOrCreateDocument(documentId, userId, date) {
   try {
@@ -202,8 +194,6 @@ function isOnlyWhiteSpace(note) {
   return /^\s*$/.test(note);
 }
 
-startServer();
-
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Server is live...");
 });
