b118331ca91207a20d69fdd6685ee90bd70dcd41
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-10-02 15:59:03 -0700

    removing next prepare()

--- index.js
+++ index.js
@@ -9,14 +9,10 @@ app.all('*', (req, res) => {
   return handle(req, res);
 })
 
-app.prepare().then(() => {
-  const server = createServer(app);
-
-  server.listen(serverPort, () => console.log(`Listening on ${serverPort}`));
-
-  module.exports = server;
-  socketHandler(server);
-});
+const server = createServer(app);
+server.listen(serverPort, () => console.log(`Listening on ${serverPort}`));
+module.exports = server;
+socketHandler(server);
 
 app.get("/", (req, res) => {
   res.send("Server is live...");
