daa62a1876dd130daa3a91fe50d392eb1f848458
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-10-23 13:58:08 -0700

    note schema only needs userid

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -3,15 +3,15 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const handleNotesData = async (req, res) => {
-  const { email, userId } = req.query;
-  if (!email || !userId) {
+  const { userId } = req.query;
+  if (!userId) {
     return res.status(400).json({
       'message': 'User info not provided.'
     });
   }
   
   try {
-    const usersNotes = await Note.find({ email, userId });
+    const usersNotes = await Note.find({ userId });
     res.json({ usersNotes: usersNotes });
   } catch (err) {
     console.error(err);
