eea6e1864d5dc6fb98d0de8c94f3b8ef0c983a79
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-10-25 19:10:28 -0700

    trying remove 404logic

--- index.js
+++ index.js
@@ -33,16 +33,6 @@ app.use('/user', require('./routes/user'));
 app.use('/notes', require('./routes/notes'));
 app.use('/logout', require('./routes/logout'));
 
-app.all('*', (req, res) => {
-  if (req.accepts('html')) {
-    res.sendFile(path.join(__dirname, 'views', '404.html'));
-  } else if (req.accepts('json')) {
-    res.json({ error: '404 Not Found' });
-  } else {
-    res.type('txt').send('404 Not Found')
-  }
-});
-
 app.use(errorHandler);
 
 mongoose.connection.once('open', () => {
